9618fd0cde718720afc6f54473f0dedd7d9cee41
Victor Andrade <victorma2000@live.com>
2021-04-21 16:17:26 -0300

    adicionando telas iniciais

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function entrarSistema(){
+    window.location = "../telas/inicio.html"
+}
+
+function novoUsuario(){
+    window.location = "../telas/criarUsuario.html"
+}
+
+function index(){
+    window.location = "../index.html"
+}
