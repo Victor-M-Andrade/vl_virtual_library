295840394d55732a30fe91961e8320bee202525f
Victor Andrade <victorma2000@live.com>
2021-10-05 15:49:07 -0300

    vl-web adicionado corretamente

---
+++ Projeto no spring/vl-web/src/main/java/br/fai/vl/web/controller/HomeController.java
@@ -0,0 +1,19 @@
+package br.fai.vl.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String getHomePage() {
+		return "home";
+	}
+
+	@GetMapping("/criadores")
+	public String getCriadores() {
+		return "general/criadores";
+	}
+
+}
