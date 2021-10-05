c6dffa3857405180259ad5557998ba1b92ba39de
LucasLima147 <lucassouzalima1478@gmail.com>
2021-10-04 21:14:09 -0300

    projeto web spring criado para preparar as páginas no padrão com o thymeleaf

---
+++ Projeto no spring/vl-web/src/main/java/br/fai/vl/web/VlWebApplication.java
@@ -0,0 +1,13 @@
+package br.fai.vl.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlWebApplication.class, args);
+	}
+
+}
